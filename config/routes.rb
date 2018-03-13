6b869c8cf3cbe51ef6b3a14a8c22eb49a2b34def
Andreia <andreia.ca22@gmail.com>
2018-03-13 10:53:23 +0000

    task 4.5

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,11 @@ Rails.application.routes.draw do
 
   get 'simple_pages/index'
 
+  root 'simple_pages#landing_page'
+  
   root 'simple_pages#index'
 
-  root 'simple_pages#landing_page'
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
