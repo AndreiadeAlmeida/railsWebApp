32bf9cf3232371852253e691511444c31db5b2ba
Andreia <andreia.ca22@gmail.com>
2018-04-04 21:05:38 +0100

    task 5.10

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,11 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.present?  # additional permissions for logged in users (they can manage their posts)
+      can :manage, User, id: user.id
+      if user.admin?  # additional permissions for administrators
+        can :manage, :all
+      end
+    end
   end
 end
