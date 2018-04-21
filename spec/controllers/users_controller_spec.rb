9c032da906ac62fb35b73afb52a45b4cffb51614
Andreia <andreia.ca22@gmail.com>
2018-04-22 00:26:03 +0100

    task 6.3

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,27 +1,33 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller do
-
-  let(:user) { User.create!(email: "user_1@testemail.com", password: "user111")}
-  let(:user) { User.create!(email: "user_2@testemail.com", password: "user222")}
+  before do
+    @user_1 = User.create!(email: "user_1@testemail.com", password: "user111")
+    @user_2 = User.create!(email: "user_2@testemail.com", password: "user222")
+  end
 
   describe 'GET #show' do
     context 'when a user is logged in' do
       before do
-        sign_in user
+        sign_in @user_1
       end
 
       it 'loads correct user details' do
-        get :show, params: {id: user.id}
+        get :show, params: {id: @user_1.id}
         expect(response).to be_ok
-        expect(assigns(:user)).to eq user
+        expect(assigns(:user)).to eq @user_1
+      end
+
+      it 'cannot access other user show page' do
+        get :show, params: {id: @user_2.id}
+        expect(response).to redirect_to(root_path)
       end
     end
 
 
     context 'when a user is not logged in' do
       it 'redirects to login' do
-        get :show, params: { id: user.id }
+        get :show, params: { id: @user_1.id }
         expect(response).to redirect_to(root_path)
       end
     end
