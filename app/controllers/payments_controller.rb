01a744c28e6f0e246b41499f59d80c8a4f2955c1
Andreia <andreia.ca22@gmail.com>
2018-05-12 15:01:04 +0100

    task 6.9 back to strip_checkout_button

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,35 +1,34 @@
 class PaymentsController < ApplicationController
-
   def create
-    #byebug
     @product = Product.find(params[:product_id])
     @user = current_user
-
     token = params[:stripeToken]
+
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
       charge = Stripe::Charge.create(
-        amount: (@product.price*100).to_i,
-        currency: "gbp",
+        amount: @product.price*100, # amount in cents, again
+        currency: "GBP",
         source: token,
         description: params[:stripeEmail],
-        receipt_email: 'andreia.ca22@gmail.com'
+        receipt_email: params[:stripeEmail]
       )
 
       if charge.paid
         Order.create(
-          product_id: @product_id,
-          user_id: @user_id,
+          product_id: @product.id,
+          user_id: @user.id,
           total: @product.price
         )
       end
+      redirect_to '/payments/success', notice: "Your payment was processed successfully. Thank you for purchasing."
 
     rescue Stripe::CardError => e
       # The card has been declined
       body = e.json_body
       err = body[:error]
-      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]} Please contact your bank"
+      redirect_to product_path(@product)
     end
-    redirect_to payments_success_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,14 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
 
   post 'simple_pages/thank_you'
-  post 'payments/create'
+
 
   get 'simple_pages/landing_page'
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
 
+  post 'payments/create'
   get 'payments/success'
 
   root 'simple_pages#landing_page'
