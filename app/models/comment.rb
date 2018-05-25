6da38a2308ee1ee70e5c1b4024cbe1f8dcaddb5b
Andreia <andreia.ca22@gmail.com>
2018-05-25 13:19:33 +0100

    task 6.14

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,12 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
-  scope :rating_desc, -> { order(rating: :desc) }
-
   validates :body, presence:true
   validates :user, presence: true
   validates :rating, numericality: {only_integer: true}
+
+  after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
+
+  scope :rating_desc, -> { order(rating: :desc) }
 
 end
