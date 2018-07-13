4e993d4236960fe527efec9b1c4085aa4a1d6f7b
Andreia <andreia.ca22@gmail.com>
2018-07-13 10:46:59 +0100

    sending payment confirmation email

---
+++ app/controllers/user_mailer.rb
@@ -0,0 +1,27 @@
+class UserMailer < ApplicationMailer
+  default from:'andreia.ca22@gmail.com'
+
+  def contact_form(email, name, message)
+    @message = message
+    mail(
+      from: email,
+      to: 'andreia.ca22@gmail.com',
+      subject: 'A new contact form message from #{name}')
+  end
+
+  def welcome(user)
+    @appname = 'BikeLondon'
+    mail(
+      to: user.email,
+      subject: 'Welcome to #{appname}!'
+    )
+  end
+
+  def payment_received(user, order)
+    @user = user
+    @order = order
+    @product = order.product
+
+    mail(to: user.email, subject: 'Thank you for purchasing!')
+  end
+end
